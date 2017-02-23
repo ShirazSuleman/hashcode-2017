7f32017879d60ff768454f318fb50fbce5c38e58
ShirazSuleman <shirazsuleman@gmail.com>
2017-02-23 06:10:49 +0200

    Made changes to use more cells when cutting.

--- Practice round/pizza_cutter.py
+++ Practice round/pizza_cutter.py
@@ -31,7 +31,8 @@ num_slices = math.ceil(pizza.get_cell_count() / metadata['max_cells_per_slice'])
 
 slices = list()
 
-min_cells = metadata['min_ingredient_per_slice'] * 2
+# min_cells = metadata['min_ingredient_per_slice'] * 2
+min_cells = metadata['max_cells_per_slice'] if metadata['rows'] > metadata['max_cells_per_slice'] else metadata['rows']
 
 column_slices = {}
 potential_column_scores = {}
@@ -44,9 +45,7 @@ while column < metadata['columns']:
         row = 0
         while row < metadata['rows']:
                 ingredient_count = {}
-                if (row + min_cells) > metadata['rows']:
-                        break
-                for cell in range(row, row + min_cells):
+                for cell in range(row, (row + min_cells) if (row + min_cells) < metadata['rows'] else metadata['rows']):
                         ingredient_count[pizza.get_cell_value((cell, column))] = ingredient_count.get(pizza.get_cell_value((cell, column)), 0) + 1
                 
                 if ingredient_count.get('T', 0) >= metadata['min_ingredient_per_slice'] and ingredient_count.get('M', 0) >= metadata['min_ingredient_per_slice']:
@@ -56,6 +55,9 @@ while column < metadata['columns']:
                 else:
                         row += 1
         column += 1
+
+# min_cells = metadata['min_ingredient_per_slice'] * 2
+min_cells = metadata['max_cells_per_slice'] if metadata['columns'] > metadata['max_cells_per_slice'] else metadata['columns']
 
 row_slices = {}
 potential_row_scores = {}
@@ -68,9 +70,7 @@ while row < metadata['rows']:
         column = 0
         while column < metadata['columns']:
                 ingredient_count = {}
-                if (column + min_cells) > metadata['columns']:
-                        break
-                for cell in range(column, column + min_cells):
+                for cell in range(column, (column + min_cells) if (column + min_cells) < metadata['columns'] else metadata['columns']):
                         ingredient_count[pizza.get_cell_value((row, cell))] = ingredient_count.get(pizza.get_cell_value((row, cell)), 0) + 1
                 
                 if ingredient_count.get('T', 0) >= metadata['min_ingredient_per_slice'] and ingredient_count.get('M', 0) >= metadata['min_ingredient_per_slice']:
